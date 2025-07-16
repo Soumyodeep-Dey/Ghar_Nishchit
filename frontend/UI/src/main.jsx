8d1b5c935b5012ecae5135cc9a0913720b3268b0
avikjana <145255757+avikjana@users.noreply.github.com>
2025-07-16 11:54:59 +0530

    clearing old files and starting new

--- frontend/UI/src/main.jsx
+++ frontend/UI/src/main.jsx
@@ -2,16 +2,13 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './layout.jsx'
-import Home from './components/home/home.jsx'
-import NotFound from './components/notFound/NotFound.jsx'
-import { ThemeProvider } from './components/contexts/theme'
+
  
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path="*" element={<NotFound />} />
+    <Route path='/' element={<Landing-Page />}>
+      <Route path='' element={<Login-Page />} />
+      
     </Route>
   )
 )
